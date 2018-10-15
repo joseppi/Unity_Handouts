e5f7e99e27bd8badce22a713450a566ba9bc3f37
Josep Pi <josep.pi.serra@gmail.com>
2018-10-15 10:30:12 +0200

    Update using recasting and nav mesh

---
+++ AI_Class_06/AI_Class_06/Class_6 Project Handout/Assets/MoveToTargetPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToTargetPosition : MonoBehaviour {
+    Move move;
+    NavMeshAgent nav_agent;
+	// Use this for initialization
+	void Start ()
+    {
+        move = GetComponent<Move>();
+        nav_agent = GetComponent<NavMeshAgent>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav_agent.destination = move.target.transform.position;
+    }
+}
