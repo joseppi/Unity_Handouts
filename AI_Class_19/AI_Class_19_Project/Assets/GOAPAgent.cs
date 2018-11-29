c73754e25979fa5ffa71aea646a90b103c45a669
Josep Pi <josep.pi.serra@gmail.com>
2018-11-29 14:17:13 +0100

    Class_19 handout implemented

---
+++ AI_Class_19/AI_Class_19_Project/Assets/GOAPAgent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GOAP;
+
+public class GOAPAgent : MonoBehaviour, IGoap {
+
+    public List<Condition> state { get; set; }
+    public List<Goal> availableGoals { get; set; }
+    public List<Action> availableActions { get; set; }
+
+    private void Awake()
+    {
+        state = new List<Condition>();
+        availableGoals = new List<Goal>();
+        availableActions = new List<Action>();
+
+        //avaliableGoals.Add(ScriptableObject.CreateInstance<GOAP_Goal_AlertBase>());
+
+        foreach(Goal g in availableGoals)
+        {
+            g.OnGoalInitialize(this);
+        }
+    }
+}
